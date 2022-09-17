17c0211bb306976358ebded4f086fd5e66af3803
kviiim <k82019@gmail.com>
2022-09-17 16:41:22 -0400

    person follower updates

--- warmup_project/warmup_project/person_follower.py
+++ warmup_project/warmup_project/person_follower.py
@@ -9,7 +9,7 @@ from rclpy.node import Node
 import numpy as np
 from matplotlib import pyplot as plt
 
-from sklearn import linear_model, datasets
+import math 
 
 rolling_avg_len = 3
 new_cluster_dist = .5
@@ -26,44 +26,53 @@ class SendTwist(Node):
 
 
     def run_loop(self):
-        angular_speed = self.angle_to_go/360
-        try:
-            linear_speed = 1.0/self.angle_to_go
-        except:
+        print(self.angle_to_go)
+
+        angular_speed = .074*math.sqrt(abs(self.angle_to_go))
+        if self.angle_to_go < 0:
+            angular_speed *= -1
+
+        if self.angle_to_go != 0:
+            linear_speed = abs(1.0/self.angle_to_go)
+        else:
             linear_speed = 1.0
-        if 360-self.angle_to_go < self.angle_to_go:
-            try:
-                linear_speed = 1.0/(360-self.angle_to_go)
-            except:
-                linear_speed = 1.0
-            angular_speed = -(360-self.angle_to_go)/360
-        
+                
 
         linear = Vector3(x=linear_speed,y=0.0,z=0.0)
-        angular = Vector3(x=0.0,y=0.0,z=-3*angular_speed)
+        angular = Vector3(x=0.0,y=0.0,z=angular_speed)
 
         cmd_vel = Twist(linear=linear,angular=angular)
         self.publisher.publish(cmd_vel)
 
     def get_laser(self,msg):
         #from https://scikit-learn.org/stable/auto_examples/linear_model/plot_ransac.html
+        shifted_ranges = msg.ranges[180:]
+        shifted_ranges.extend(msg.ranges[:180])
+
         current_cluster = {'start': rolling_avg_len+1, 'end': rolling_avg_len+1}
         largest_cluster = {'start': rolling_avg_len+1, 'end': rolling_avg_len+1}
 
-        for curr_idx in range(rolling_avg_len+1, len(msg.ranges)):
-            last_avg = mean(msg.ranges[curr_idx-rolling_avg_len-1:curr_idx-1])
-            curr_avg = mean(msg.ranges[curr_idx-rolling_avg_len:curr_idx])
+        for curr_idx in range((rolling_avg_len//2) + 1, len(msg.ranges)-rolling_avg_len//2):
 
-            if abs(curr_avg - last_avg) > new_cluster_dist:
+            last_avg = mean(shifted_ranges[(curr_idx-(rolling_avg_len//2))-1 : (curr_idx+(rolling_avg_len//2))])
+            curr_avg = mean(shifted_ranges[(curr_idx-rolling_avg_len//2) : (curr_idx+rolling_avg_len//2) + 1])
+            if last_avg == float('inf'):
+                last_avg = 0
+            if curr_avg == float('inf'):
+                curr_avg = 0
+
+            if abs(curr_avg - last_avg) > new_cluster_dist :
                 #make new cluster
-                current_cluster['start'] = curr_idx
-                current_cluster['end'] = curr_idx
-            else:
-                current_cluster['end'] = curr_idx
+                current_cluster['start'] = curr_idx-180
+                current_cluster['end'] = curr_idx-180
+            elif curr_avg > .1:
+                current_cluster['end'] = curr_idx-180
                 if current_cluster['end'] - current_cluster['start'] > largest_cluster['end'] - largest_cluster['start']:
                     largest_cluster = current_cluster
-        
-        self.angle_to_go = mean([largest_cluster['end'],largest_cluster['start'] - 3])
+        print(largest_cluster)
+        self.angle_to_go = mean([largest_cluster['end'],largest_cluster['start']])
+        if self.angle_to_go > 360:
+            self.angle_to_go = self.angle_to_go - 360
 
 
 def main(args=None):
